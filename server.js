a7a7c5473e30042b6e5982a3ddc22e7708a24d0b
dalythu <dalythu@gmail.com>
2026-02-09 21:53:59 -0500

    Initialize server, middleware, and MongoDB connection

---
+++ server.js
@@ -0,0 +1,25 @@
+// Imports
+import express from 'express'
+import dotenv from 'dotenv'
+import { logReq, globalErr } from './middleware/middlewares.js'
+import connectDB from './db/conn.js'
+
+// Setups
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3001
+connectDB()
+
+// Middleware
+app.use(express.json())
+app.use(logReq)
+
+// Route
+
+// Global Err
+app.use(globalErr)
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server Listening on PORT: ${PORT}`)
+})
